594d7804d14ccf8f5e2e0c00958c4dc5afe28b1d
sat6362255029 <sat6362255029@gmail.com>
2023-08-22 23:44:41 +0530

    Hellow world code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+        ),
+      ),
     ));
