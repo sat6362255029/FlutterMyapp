e2adad46b2e4faf7f6b6cd28232f56379362a2a9
sat6362255029 <sat6362255029@gmail.com>
2023-08-23 16:02:16 +0530

    image widget inserted used local image

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Image(
-          image: NetworkImage(
-              'https://images.unsplash.com/photo-1542480118439-cbba7870f6f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80'),
-          // child: Text('Hello World',
-          //     style: TextStyle(
-          //       fontSize: 20.0,
-          //       fontWeight: FontWeight.bold,
-          //       letterSpacing: 2.0,
-          //       color: Colors.grey,
-          //       fontFamily: 'IndieFlower',
+          image: AssetImage('assets/photo-2.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
