75cd8e66738d1c2b37c105e1649716a57a40b1d2
sat6362255029 <sat6362255029@gmail.com>
2023-08-23 09:31:34 +0530

    updated the appbar colour and font

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-        ),
-      ),
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext Context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text('Hello World',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlower',
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
